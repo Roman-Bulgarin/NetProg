d7fd1b2d2a5eb31255e6bd4d9b28e41dde18230f
Roman-Bulgarin <147330001+Roman-Bulgarin@users.noreply.github.com>
2023-12-27 03:17:25 +0300

    main.cpp

--- echo/main.cpp
+++ echo/main.cpp
@@ -1,67 +1,55 @@
 #include <iostream>
-#include <string>
-#include <cstdlib> 
-#include <cstring> 
-#include <unistd.h> 
-#include <netinet/in.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+
 using namespace std;
-void Exception(const string & why, const int exitCode ) 
+//Определение размера буфера с помощью макроса
+#define BUFSZ 128
+int main(int argc, char *argv[])
 {
-    cout << "ErrorCode:"<<exitCode <<endl<< why << endl;
-    exit(exitCode);
-}
-int main()
-{
-    sockaddr_in * selfAddr = new (sockaddr_in);
-    selfAddr->sin_family = AF_INET; 
-    selfAddr->sin_port = 0;         
-    selfAddr->sin_addr.s_addr = 0;  
-
-    sockaddr_in * remoteAddr = new (sockaddr_in);
-    remoteAddr->sin_family = AF_INET;     
-    remoteAddr->sin_port = htons(44214); 
-    remoteAddr->sin_addr.s_addr = inet_addr("127.0.0.1"); 
-   
-    char *buffer = new char[4096];
-    strcpy(buffer,"Привет, как дела?");  
-    int msgLen = strlen(buffer);           
-   
-    int mySocket = socket(AF_INET, SOCK_STREAM, 0); 
-    if (mySocket == -1) {
-        close(mySocket);
-        Exception("Ошибка открытия сокета",11);
+    int sock; //Сокет
+    char buffer[BUFSZ]; //Буфер для приема ответа сервера
+    struct sockaddr_in server; //Содержит информацию о сервере
+// Создание сокета
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        cout << "Failed to create socket" << endl;
+        exit(1);
     }
-    int rc = bind(mySocket,(const sockaddr *) selfAddr, sizeof(sockaddr_in));
-    if (rc == -1) {
-        close(mySocket);
-        Exception("Ошибка привязки сокета с локальным адресом",12);
-        }
-
-    rc = connect(mySocket, (const sockaddr*) remoteAddr, sizeof(sockaddr_in));
-    if (rc == -1) {
-        close(mySocket);
-        Exception("Ошибка подключения сокета к удаленному серверу.", 13);
+// Настройка адреса сервера
+    server.sin_family = AF_INET;
+    server.sin_port = htons(7);
+    server.sin_addr.s_addr = inet_addr("82.179.90.12");//inet_addr("127.0.0.1");
+	
+// Подключение к серверу
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        cout << "Error connecting to the server" << endl;
+        exit(1);
     }
-
-    rc = send(mySocket, buffer, msgLen, 0);
-    if (rc == -1) {
-        close(mySocket);
-        Exception("Сообщение об ошибке отправки", 14);
+//Отправка запроса серверу
+	string message;
+	cout << "Enter the message to send: ";
+	getline(cin, message);
+	
+    if(send(sock, message.c_str(), message.size(), 0) < 0) {
+        cout<< "Error sending the request" << endl;
+        exit(1);
     }
-    cout << "Мы отправляем: " << buffer << endl; 
-      
-    rc = recv(mySocket, buffer, 4096, 0);
-    if (rc == -1) {
-        close(mySocket);
-       Exception("Ошибка получения ответа.", 15);
+    
+// Получение ответа от сервера
+    int num_bytes = recv(sock, buffer, sizeof(buffer), 0);
+    if(num_bytes < 0){
+        cout << "Error when receiving a response from the server" << endl;
+        exit(1);
     }
-    buffer[rc] = '\0'; 
-    cout << "Мы получаем: " << buffer << endl;
-    close(mySocket);
-
-    delete selfAddr;
-    delete remoteAddr;
-    delete[] buffer;
-    return 0;
+// Вывод результата
+    cout << "Message: " << endl;
+    buffer[num_bytes] = '\0';
+    cout << buffer << endl;
+// Закрытие сокета
+    close(sock);
 }
